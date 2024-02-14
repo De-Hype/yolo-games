e061ce4c79a2c53fb501ffefb2e2ded18c2cdbf3
De-Hype <iamsocialhype@gmail.com>
2024-02-13 18:09:30 -0800

    Added the text at top and made the keyframe dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,17 @@ import Dice from "./Components/Dice";
 
 function App() {
   const [rollClicked, setRollClicked] = useState(false);
-  const [number, setNumber]=useState(null);
-  const [trajectoryPosition, setTrajectoryPosition] = useState({right:'50',})
-
+  const [number, setNumber] = useState(50);
+  const animationName = number > 50 ? "dotmoveRight" : "dotmoveLeft";
+ 
   const [inputs, setInputs] = useState({
     multiplierInput: 1.98,
     rolloverInput: 50,
     chanceInput: 50,
   });
   const GenerateRandomNumber = () => {
-    const randomNumber = parseFloat(((Math.random() * 99) + 1).toFixed(2));
-alert(`The randomly generated number is ${randomNumber}`)
-    const randomRight = randomNumber + 'px';
-    setTrajectoryPosition({top:randomRight})
+    const randomNumber = parseFloat((Math.random() * 99 + 1).toFixed(2));
+    setNumber(randomNumber);
   };
 
   const handleInputChange = (inputName, value) => {
@@ -36,7 +34,26 @@ alert(`The randomly generated number is ${randomNumber}`)
       rolloverInput: 100 - inputs.rolloverInput,
     }));
   };
-  
+  const stayUpPosition = {
+    backgroundColor: number > 50 ? "lime" : "red",
+    fontSize: "14px",
+    padding: "2px 4px",
+    position: "absolute",
+    width:'40px',
+    top: "10px",
+    right: `-calc(${number}% + 5%)`,
+    left: `calc(${number}% - 4%)`,
+  };
+  const GoUpPosition = {
+    color: number > 50 ? "lime" : "red",
+    fontSize: "14px",
+    padding: "2px 4px",
+    position: "absolute",
+    right: `-calc(${number}% + 5%)`,
+    left: `calc(${number}% - 4%)`,
+    display:'none',
+  };
+
   const dotMoveAnimation = {
     popping: {
       width: "5px",
@@ -47,36 +64,60 @@ alert(`The randomly generated number is ${randomNumber}`)
       top: "50%",
       bottom: "50%",
       zIndex: "999999999",
-      animation: "dotmove 6s linear  infinite",
+      animation: `${animationName} 2s infinite forwards`,
     },
   };
   return (
     <>
+      {/* right:${200}px; */}
       <style>
-        {` @keyframes dotmove {
+        {` @keyframes dotmoveRight {
           0% {
               top: 50%;
               bottom: 50%;
               right: 50%;
+              left: ${50}%;
             }
             20% {
-              right:200px;
+              right: -calc(${number}% + 5%);
+              left: calc(${number}% - 4%);
               top: 50%;
               bottom: 50%;
             }
             40% {
               top: 80%;
             }
-            100% {
-              top: 80%;
+            
+          }
+          @keyframes dotmoveLeft {
+            0% {
+                top: 50%;
+                bottom: 50%;
+                right: 50%;
+                left: ${50}%;
+              }
+              20% {
+
+                left: calc(${number}% + 2%);
+                top: 50%;
+                bottom: 50%;
+              }
+              40% {
+                top: 80%;
+              }
+              
             }
-          }`}
+          `}
       </style>
       <div className="min-h-screen w-screen overflow-x-hidden bg-black text-white flex items-center justify-center">
-        <div className="w-2/3 h-full flex flex-col gap-14">
-          <div className="outer-container relative">
+        <div className="w-2/3 relative h-full flex flex-col gap-14">
+          <div className="outer-container ">
             {/* <div className="dot-popping"></div> */}
             <div style={dotMoveAnimation.popping}></div>
+            <div style={GoUpPosition} className="go-up">{number}</div>
+            <h3 style={stayUpPosition} className="transition">
+              {number}
+            </h3>
             <Dice GenerateRandomNumber={GenerateRandomNumber} />
             <div className="py-12 px-2 h-20 w-full    shadow-show relative rounded-md">
               <ColoredBox
