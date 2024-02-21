9315618491491ae192319ae10b0f54f1efd4bba9
De-Hype <iamsocialhype@gmail.com>
2024-02-20 19:40:36 -0800

    Added the statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import  Search  from "./Components/Search";
 import  Statistics  from "./Components/Statistics";
 import  UserInfo  from "./Components/UserInfo";
+import './App.css'
 
 const App = () => {
   return (
-    <div className="bg-gray-900 min-h-screen px-2 pt-2">
+    <div className="bg-stone-800 min-h-screen px-2 pt-2">
       <Search />
       <UserInfo />
       <Statistics />
--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,8 +1,55 @@
-
+import { FaLock } from "react-icons/fa";
 const Statistics = () => {
   return (
-    <div className="mt-2">Statistics</div>
-  )
-}
+    <div className="mt-2 green px-3 py-6 rounded">
+      <h3 className="text-white font-semibold pb-5 text-lg">Statistics</h3>
+      <div className="text-white px-5">
+        <div className=" px-2 py-1 flex items-center justify-between bg-orange-950">
+          <h3 className="">Wagered</h3>
+          <p>
+            $<span>12,690, 842</span>
+          </p>
+        </div>
+        <div className="px-2 py-1  flex items-center justify-between">
+          <h3 className="">Bets</h3>
+          <p>4,480</p>
+        </div>
+        <div className="px-2 py-1  flex items-center justify-between bg-orange-950">
+          <h3 className="">Bets Won</h3>
+          <p>1,780</p>
+        </div>
+        <div className="px-2 py-1  flex items-center justify-between">
+          <h3 className="">Bets Lost</h3>
+          <p>2,780</p>
+        </div>
+        <div className=" px-2 py-1 flex items-center justify-between bg-orange-950">
+          <h3 className="">Highest Win</h3>
+          <p>
+            $<span>2, 246</span>
+          </p>
+        </div>
+        <div className="px-2 py-1  flex items-center justify-between">
+          <h3 className="">Highest Multiplier</h3>
+          <p>2x</p>
+        </div>
+        <div className=" px-2 py-1 flex items-center justify-between bg-orange-950">
+          <h3 className="">Favourite Game</h3>
+          <p className="flex items-center">
+            <span>
+              <FaLock className="text-red-500"/>
+            </span>
+            <span>Plinko</span>
+          </p>
+        </div>
+        <div className=" px-2 py-1 flex items-center justify-between">
+          <h3 className="">Gross Profit</h3>
+          <p className="text-green-400">
+            $<span>1,690, 842</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Statistics
+export default Statistics;
