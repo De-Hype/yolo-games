ec2bbd4219a1702c093a502c74af88ebf501cd89
De-Hype <iamsocialhype@gmail.com>
2024-03-17 13:43:40 -0700

    Retouch responsiveness

--- src/Components/PrizeBox.jsx
+++ src/Components/PrizeBox.jsx
@@ -7,7 +7,7 @@ const PrizeBox = () => {
             className="bg-yellow-200 h-14 w-14 px-1 py-2   flex items-center justify-center"
             id="hexa-shape"
           >
-            <h3 className="text-yellow-200 bg-black flex items-center justify-center h-10 w-10 rounded-full font-semibold text-3xl">
+            <h3 className="text-yellow-200 bg-black flex items-center sm:text-xl justify-center h-10 w-10 rounded-full font-semibold text-3xl">
               1
             </h3>
           </div>
@@ -16,32 +16,32 @@ const PrizeBox = () => {
         </div>
       </div>
       <div className="flex items-center gap-2">
-        <div className="flex w-full green-radial-bg items-center pl-5 gap-7 border border-slate-700 rounded-md px-2 py-1">
+        <div className="flex w-full green-radial-bg items-center pl-5 gap-7 sm:gap-3 sm:pl-2 border border-slate-700 rounded-md px-2 py-1">
           <div
-            className="bg-green-300 h-10 w-10 px-1 py-2 flex items-center justify-center"
+            className="bg-green-300 h-10 w-10 sm:h-8 sm:w-8 px-1 py-2 flex items-center justify-center"
             id="hexa-shape"
           >
-            <h3 className="text-green-300 bg-black flex items-center justify-center h-8 w-8 rounded-full font-semibold text-3xl">
+            <h3 className="text-green-300 bg-black flex items-center sm:h-5 sm:text-xl sm:w-5 justify-center h-8 w-8 rounded-full font-semibold text-3xl">
               2
             </h3>
           </div>
           <div className="flex items-center flex-col">
-            <h3 className="text-green-300 font-bold text-lg ">78,589</h3>
-            <p className="text-slate-400 text-sm">$1,448.75</p>
+            <h3 className="text-green-300 font-bold text-lg md:text-base">78,589</h3>
+            <p className="text-slate-400 text-sm md:text-xs">$1,448.75</p>
           </div>
         </div>
-        <div className="flex w-full orange-radial-bg items-center pl-5 gap-7 border border-slate-700 rounded-md px-2 py-1 ">
+        <div className="flex w-full orange-radial-bg items-center pl-5 gap-7 sm:gap-3  sm:pl-2 border border-slate-700 rounded-md px-2 py-1 ">
           <div
-            className="bg-orange-400 h-11 w-11 px-1 py-2 flex items-center justify-center"
+            className="bg-orange-400 h-11 w-11 sm:h-8 sm:w-8 px-1 py-2 flex items-center justify-center"
             id="hexa-shape"
           >
-            <h3 className="text-orange-400 bg-black flex items-center justify-center h-8 w-8 rounded-full font-semibold text-3xl">
+            <h3 className="text-orange-400 bg-black flex sm:text-xl items-center justify-center sm:h-5 sm:w-5 h-8 w-8 rounded-full font-semibold text-3xl">
               3
             </h3>
           </div>
           <div className="flex items-center flex-col">
-            <h3 className="text-orange-400 font-bold text-lg ">78,589</h3>
-            <p className="text-slate-400 text-sm">$1,448.75</p>
+            <h3 className="text-orange-400 font-bold text-lg md:text-base ">78,589</h3>
+            <p className="text-slate-400 text-sm md:text-xs">$1,448.75</p>
           </div>
         </div>
       </div>
--- src/Layout/LotteryHeader.jsx
+++ src/Layout/LotteryHeader.jsx
@@ -1,7 +1,7 @@
 const LotteryHeader = () => {
   return (
     <div className="flex items-center md:block  justify-between">
-      <h3 className="font-black text-white text-7xl">LOTTERY</h3> 
+      <h3 className="font-black text-white text-7xl md:text-5xl">LOTTERY</h3> 
       <button type="button" className="bg-white md:mt-6 font-bold text-sm px-4 py-3 rounded-lg">Connect Wallet</button>
     </div>
   );
